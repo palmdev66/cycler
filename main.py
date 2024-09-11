ab1be0c4c22f2bac0787f71d5841f5e38efcc9f9
palmware <flowner3d@gmail.com>
2024-09-11 05:11:18 +0300

    .
    .

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import time
 import subprocess
+import os
 
-# Укажите путь к вашей рабочей папке
-working_directory = "/путь/к/вашей/рабочей/папке"
+# Получите путь к текущей рабочей папке
+working_directory = os.getcwd()
 
 command = ["yarn", "cli", "mint", "-i", "45ee725c2c5993b3e4d308842d87e973bf1951f5f7a804b21e4dd964ecd12d6b_0", "5"]
 
 while True:
     subprocess.run(command, cwd=working_directory)
-    time.sleep(30)
+    time.sleep(10)
